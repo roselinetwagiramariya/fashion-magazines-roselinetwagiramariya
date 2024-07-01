f7e8b820058161f7a3c307cb9a0b730c8bdf9047
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-07-01 15:07:58 -0400

    Update fashion_magazines.sql

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,15 @@
--- Add your sql here
+-- Add SELECT 
+    c.customer_name AS "Customer Name", 
+    PRINTF('%.2f', SUM(s.price_per_month * s.subscription_length)) AS "Total Amount Due"
+FROM 
+    orders o
+JOIN 
+    customers c ON o.customer_id = c.customer_id
+JOIN 
+    subscriptions s ON o.subscription_id = s.subscription_id
+WHERE 
+    o.order_status = 'unpaid' AND 
+    s.description = 'Fashion Magazine'
+GROUP BY 
+    c.customer_name;
+ sql here
